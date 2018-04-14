7e7466d2d2921cf4a5fc4b99eec87b0f3c2b5fe6
王会鹏 <wanghuipeng@fangjinsuo.com>
2018-04-14 17:19:59 +0800

    配置sass

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="toolbar">
-    toolbar<i class="fa fa-address-book"></i>
+    <i class="fa fa-plus" @click="addNote"></i>
+    <i :class="favorite ? 'fa fa-heart' : 'fa fa-heart-o'" @click="likeNote"></i>
+    <i class="fa fa-remove" @click="delNote"></i>
   </div>
 </template>
 
@@ -8,12 +10,29 @@
 export default {
   data () {
     return {
+      favorite: false
+    }
+  },
+  methods: {
+    addNote() {
+      console.log('新建note')
+    },
+    likeNote() {
+      this.favorite = !this.favorite;
+      console.log('收藏note')
+    },
+    delNote() {
+      console.log('删除note')
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style scoped lang="scss">
+.toolbar{
+   position: absolute;
+   top: 0;
+   border-right: 1px solid gray;
+}
 </style>
